b2ce5ccae15dbd6831f979e14951fa36ab10bf5e
David Starr <dstarrtrey@gmail.com>
2019-01-27 20:41:12 -0800

    edits

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   //THIS IS SIDEWAYS
-  const chessboard = {
+  const chessboardTemplate = {
     a: [1, 2, 3, 4, 5, 6, 7, 8],
     b: [1, 2, 3, 4, 5, 6, 7, 8],
     c: [1, 2, 3, 4, 5, 6, 7, 8],
@@ -11,164 +11,137 @@ $(document).ready(function() {
     h: [1, 2, 3, 4, 5, 6, 7, 8]
   };
   const chessboardStart = {
-    a: ["wr1", "wp1", 3, 4, 5, 6, "bp1", "br1"],
-    b: ["wk1", "wp2", 3, 4, 5, 6, "bp2", "bk1"],
-    c: ["wb1", "wp3", 3, 4, 5, 6, "bp3", "bb1"],
-    d: ["wq", "wp4", 3, 4, 5, 6, "bp4", "bki"],
-    e: ["wki", "wp5", 3, 4, 5, 6, "bp5", "bq"],
-    f: ["wb2", "wp6", 3, 4, 5, 6, "bp6", "bb2"],
-    g: ["wk2", "wp7", 3, 4, 5, 6, "bp7", "bk2"],
-    h: ["wr2", "wp8", 3, 4, 5, 6, "bp8", "br2"]
+    a: ["wr1", "wp1", "empty", "empty", "empty", "empty", "bp1", "br1"],
+    b: ["wk1", "wp2", "empty", "empty", "empty", "empty", "bp2", "bk1"],
+    c: ["wb1", "wp3", "empty", "empty", "empty", "empty", "bp3", "bb1"],
+    d: ["wq", "wp4", "empty", "empty", "empty", "empty", "bp4", "bki"],
+    e: ["wki", "wp5", "empty", "empty", "empty", "empty", "bp5", "bq"],
+    f: ["wb2", "wp6", "empty", "empty", "empty", "empty", "bp6", "bb2"],
+    g: ["wk2", "wp7", "empty", "empty", "empty", "empty", "bp7", "bk2"],
+    h: ["wr2", "wp8", "empty", "empty", "empty", "empty", "bp8", "br2"]
   };
+  let chessboard = JSON.parse(JSON.stringify(chessboardStart));
   const letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
   const PIECES = {
     wp1: {
       img: "assets/images/wp.png",
       start: "a2",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     wp2: {
       img: "assets/images/wp.png",
       start: "b2",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     wp3: {
       img: "assets/images/wp.png",
       start: "c2",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     wp4: {
       img: "assets/images/wp.png",
       start: "d2",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     wp5: {
       img: "assets/images/wp.png",
       start: "e2",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     wp6: {
       img: "assets/images/wp.png",
       start: "f2",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     wp7: {
       img: "assets/images/wp.png",
       start: "g2",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     wp8: {
       img: "assets/images/wp.png",
       start: "h2",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     bp1: {
       img: "assets/images/bp.png",
       start: "a7",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     bp2: {
       img: "assets/images/bp.png",
       start: "b7",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     bp3: {
       img: "assets/images/bp.png",
       start: "c7",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     bp4: {
       img: "assets/images/bp.png",
       start: "d7",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     bp5: {
       img: "assets/images/bp.png",
       start: "e7",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     bp6: {
       img: "assets/images/bp.png",
       start: "f7",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     bp7: {
       img: "assets/images/bp.png",
       start: "g7",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     bp8: {
       img: "assets/images/bp.png",
       start: "h7",
       type: "pawn",
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
+      hasBeenMoved: false
     },
     wr1: {
       img: "assets/images/wr.png",
       start: "a1",
-      type: "rook"
+      type: "rook",
+      hasBeenMoved: false
     },
     wr2: {
       img: "assets/images/wr.png",
       start: "h1",
-      type: "rook"
+      type: "rook",
+      hasBeenMoved: false
     },
     br1: {
       img: "assets/images/br.png",
       start: "a8",
-      type: "rook"
+      type: "rook",
+      hasBeenMoved: false
     },
     br2: {
       img: "assets/images/br.png",
       start: "h8",
-      type: "rook"
+      type: "rook",
+      hasBeenMoved: false
     },
     wk1: {
       img: "assets/images/wk.png",
@@ -223,13 +196,60 @@ $(document).ready(function() {
     wki: {
       img: "assets/images/wki.png",
       start: "e1",
-      type: "king"
+      type: "king",
+      hasBeenMoved: false
     },
     bki: {
       img: "assets/images/bki.png",
       start: "d8",
-      type: "king"
+      type: "king",
+      hasBeenMoved: false
     }
+  };
+  const WHITEMOVEMENTOPTIONS = {
+    pawn: (piece, location) => {
+      let locArr = location.split("");
+      let x = locArr[0];
+      let y = locArr[1] - 1;
+      let moveArr = [];
+      if (chessboard[x][y + 1] === "empty") {
+        if (y + 1 < 8) {
+          moveArr.push([x, y + 1]);
+        }
+        if (piece.hasBeenMoved === false && chessboard[x][y + 2] === "empty") {
+          moveArr.push([x, y + 2]);
+        }
+      }
+      //capture/a passant
+      if (x !== "a") {
+        if (
+          chessboard[letters[letters.indexOf(x) - 1]][y + 1].charAt(0) ===
+            "b" ||
+          PIECES[chessboard[letters[letters.indexOf(x) - 1]][y]]
+            .hasBeenMoved === "justMoved"
+        ) {
+          moveArr.push([letters[letters.indexOf(x) - 1], y + 1]);
+        }
+      }
+      //capture/a passant
+      if (x !== "h") {
+        if (
+          chessboard[letters[letters.indexOf(x) + 1]][y + 1][0] === "b" ||
+          PIECES[chessboard[letters[letters.indexOf(x) + 1]][y]]
+            .hasBeenMoved === "justMoved"
+        ) {
+          moveArr.push([letters[letters.indexOf(x) + 1], y + 1]);
+        }
+      }
+      return moveArr;
+    }
+  };
+  const showMovementOptions = arr => {
+    $(".hover").removeClass("hover");
+    arr.forEach(opt => {
+      opt[1]++;
+      $(`#${opt.join("")}`).addClass("hover");
+    });
   };
   const setUpGame = () => {
     Object.keys(PIECES).forEach(piece => {
@@ -241,38 +261,6 @@ $(document).ready(function() {
       $(`#${currentPiece.start}`).html(imgFill);
     });
   };
-  const MOVEMENT = {
-    pawn: {
-      moveOptions: piece => {
-        if (piece.canCapture === false) {
-          if (piece.blocked === false) {
-            if (piece.hasBeenMoved === false) {
-              return [[0, 1], [0, 2]];
-            } else {
-              return [[0, 1]];
-            }
-          } else {
-            return [[1, 1]];
-          }
-        }
-      },
-      blocked: false,
-      hasBeenMoved: false,
-      canCapture: false
-    }
-  };
-  const showMovementOptions = (piece, position) => {
-    const optionsArr = MOVEMENT[piece.type].moveOptions(piece);
-    for (let x = 0; x < optionsArr.length; x++) {
-      let optionX = optionsArr[x][0];
-      let optionY = optionsArr[x][1];
-      let newXY =
-        letters[letters.indexOf(position[0]) + optionX] +
-        (parseInt(position[1]) + optionY);
-      console.log(newXY);
-      $(`#${newXY}`).addClass("hover");
-    }
-  };
   let myTurn = true;
   let myColor = "white";
   setUpGame();
@@ -281,11 +269,15 @@ $(document).ready(function() {
     if (myTurn === true && myColor === "white") {
       if ($(this).hasClass("w")) {
         const thisPiece = PIECES[$(this).attr("data-occupying")];
-        showMovementOptions(thisPiece, coordinate);
-        console.log(thisPiece);
+        console.log(
+          WHITEMOVEMENTOPTIONS[thisPiece.type](thisPiece, coordinate)
+        );
+        showMovementOptions(
+          WHITEMOVEMENTOPTIONS[thisPiece.type](thisPiece, coordinate)
+        );
       }
-    } else if (myTurn === true && myColor === "b") {
-      if ($(this).hasClass("black")) {
+    } else if (myTurn === true && myColor === "black") {
+      if ($(this).hasClass("b")) {
         //
       }
     }
